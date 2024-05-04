d78d635481ee9c665e3d49e4d4cebf44cf86c526
Mao_Thaknita <thaknita555@gmail.com>
2024-05-04 10:59:27 +0700

    to deploy

--- src/main/java/com/springboot/minimartapi/PublicController.java
+++ src/main/java/com/springboot/minimartapi/PublicController.java
@@ -1,8 +1,7 @@
-package com.springboot.elearningmanagementstructurebyfeature.publicapi;
+package com.springboot.minimartapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 
 @RestController
